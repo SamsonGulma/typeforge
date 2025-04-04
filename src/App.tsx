b15888be6ed8605be9792104cfb0171a36e60510
SamsonGulma <sams0ngultek@gmail.com>
2025-04-04 16:35:31 +0300

    learn more about typing.

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,13 @@
 import './App.css'
 import { Person } from './Person';
 
-let name: string = "Samson";
-let age: number = 22;
-let isBeliver: boolean = true;
-
-let ages: number[] = [1, 2, 3, 4, 5];
-let person: any = 3;
 
 function App() {
+
   return (
     <>
-      <Person/>
+      <Person name={"Samson"} age={22} isBeliver={true} />
+      <Person name={"Unknown"} age={34} isBeliver={false} />
     </>
   )
 }
--- src/Person.tsx
+++ src/Person.tsx
@@ -1,5 +1,13 @@
+export interface Person {
+    name: string;
+    age: number;
+    isBeliver: boolean;
+}
 
-
-export const Person = () => {
-    return <div>This is a person</div>
+export const Person = (props: Person) => {
+    return <div>
+        <p>My Name is {props.name}</p>
+        <p>I am {props.age} years old</p>
+        <p>Yes, I am a {props.isBeliver ? "Beliver" : "Non-Beliver"}</p>
+        </div>
 }
